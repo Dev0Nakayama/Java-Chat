4b748b78066b18cefdf784b3cd733d44ced2ea04
Dev0Nakayama <89699349+Dev0Nakayama@users.noreply.github.com>
2021-08-30 16:19:48 -0300

    Primeira verção - Chat Java(Comentado)
    Adicionado comentarios de explicação

--- Cliente/src/Cliente/Cliente.java
+++ Cliente/src/Cliente/Cliente.java
@@ -6,14 +6,23 @@ import java.util.Scanner;
 
 public class Cliente {
 	public static void main(String[] args) throws UnknownHostException, IOException{
-		@SuppressWarnings("resource")
-		Socket s = new Socket("localhost",49999);
-		PrintWriter pr = new PrintWriter(s.getOutputStream());
-		Scanner sc = new Scanner(System.in);
-		InputStreamReader in = new InputStreamReader(s.getInputStream());
-		BufferedReader bf = new BufferedReader(in);
-		System.out.println("Server Conectado");
 		
+		//declara que s prescisa manda ligação ao ip e porta
+		Socket s = new Socket("localhost",49999);
+
+		//scanner para entrada de informacao
+		Scanner sc = new Scanner(System.in);
+
+		//cria um fluxo leitor para o socket em bytes
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+		
+		//tras o fluxo leitor para caracteres
+		BufferedReader bf = new BufferedReader(in);
+
+		//envia fluxo de dados
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+
+		System.out.println("Server Conectado");		
 		while(true) {
 		//recebe
 		String str = bf.readLine();
--- Serve/src/Serve/Serve.java
+++ Serve/src/Serve/Serve.java
@@ -7,14 +7,25 @@ import java.util.Scanner;
 public class Serve {
 	public static void main(String[] args) throws IOException {
 		
+		//declara que ss prescisa escutar as ligaçoes da porta 49999
 		ServerSocket ss = new ServerSocket(49999);
-		Socket s = ss.accept();
-		Scanner sc = new Scanner(System.in);
-		InputStreamReader in = new InputStreamReader(s.getInputStream());
-		BufferedReader bf = new BufferedReader(in);
-		PrintWriter pr = new PrintWriter(s.getOutputStream());
-		System.out.println("Cliente Conectado");
 
+		//espera uma coneção de algum lugar
+		Socket s = ss.accept();
+
+		//scanner para entrada de dados
+		Scanner sc = new Scanner(System.in);
+
+		//cria um fluxo leitor para o socket em bytes
+		InputStreamReader in = new InputStreamReader(s.getInputStream());
+
+		//tras o fluxo leitor para caracteres
+		BufferedReader bf = new BufferedReader(in);
+
+		//envia fluxo de dados
+		PrintWriter pr = new PrintWriter(s.getOutputStream());
+
+		System.out.println("Cliente Conectado");
 		while(true) {	
 			//manda
 			String say = sc.next();
